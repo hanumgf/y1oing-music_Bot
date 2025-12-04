f0a13789bd7b6951d497224cf9d7f494dd708103
hanumgf <plside.best.sead@gmail.com>
2025-12-04 22:07:37 +0900

    support systemd apps

--- y1oing-music_Bot/bot/main.py
+++ y1oing-music_Bot/bot/main.py
@@ -9,10 +9,13 @@
 # バックグラウンドプロセスの正常なシャットダウンを管理します。
 
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from .client import Y1oingBot
 from utils.audio_handler import executor
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+os.chdir(BASE_DIR)
 
 def main():
     # Load environment variables from the .env file.
